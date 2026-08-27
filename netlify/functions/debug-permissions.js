f324c2662385c0311a3ce2ceb314e64a4289e605
Kaan Tan <46690252+Kaaantn@users.noreply.github.com>
2026-08-27 09:20:08 +0300

    Debug: token uzunluk/karakter kontrolü

--- netlify/functions/debug-permissions.js
+++ netlify/functions/debug-permissions.js
@@ -6,17 +6,21 @@ exports.handler = async function () {
     return { statusCode: 500, body: JSON.stringify({ error: "IG_PAGE_ACCESS_TOKEN not set" }) };
   }
 
-  const meUrl = `https://graph.facebook.com/${GRAPH_VERSION}/me?fields=id,username&access_token=${token}`;
-  const debugUrl = `https://graph.facebook.com/${GRAPH_VERSION}/debug_token?input_token=${token}&access_token=${token}`;
+  const debugInfo = {
+    length: token.length,
+    prefix: token.slice(0, 8),
+    suffix: token.slice(-6),
+    hasWhitespace: /\s/.test(token),
+    hasQuotes: /["']/.test(token),
+    dotCount: (token.match(/\./g) || []).length,
+  };
 
-  const [meRes, debugRes] = await Promise.all([
-    fetch(meUrl).then((r) => r.json()).catch((e) => ({ error: String(e) })),
-    fetch(debugUrl).then((r) => r.json()).catch((e) => ({ error: String(e) })),
-  ]);
+  const meUrl = `https://graph.facebook.com/${GRAPH_VERSION}/me?fields=id,username&access_token=${token}`;
+  const meRes = await fetch(meUrl).then((r) => r.json()).catch((e) => ({ error: String(e) }));
 
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ me: meRes, debug: debugRes }, null, 2),
+    body: JSON.stringify({ debugInfo, me: meRes }, null, 2),
   };
 };

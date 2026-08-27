826a068a1087e62a0eac3d6d7b754c9cdedce7d4
Kaan Tan <46690252+Kaaantn@users.noreply.github.com>
2026-08-27 09:29:00 +0300

    Debug: Reels dışı gönderi ve yorum testi

--- netlify/functions/debug-permissions.js
+++ netlify/functions/debug-permissions.js
@@ -15,17 +15,44 @@ exports.handler = async function () {
     dotCount: (token.match(/\./g) || []).length,
   };
 
-  const testCommentId = "18062389934766206";
-  const replyUrl = `https://graph.instagram.com/${GRAPH_VERSION}/${testCommentId}/private_replies?access_token=${token}`;
-  const replyRes = await fetch(replyUrl, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: { text: "Test" } }),
-  }).then((r) => r.json()).catch((e) => ({ error: String(e) }));
+  const mediaUrl = `https://graph.instagram.com/${GRAPH_VERSION}/me/media?fields=id,media_type,media_product_type,permalink&limit=25&access_token=${token}`;
+  const mediaRes = await fetch(mediaUrl).then((r) => r.json()).catch((e) => ({ error: String(e) }));
+
+  const nonReel = (mediaRes.data || []).find((m) => m.media_product_type !== "REELS");
+
+  let nonReelComment = null;
+  let nonReelReplyTest = null;
+  if (nonReel) {
+    const commentsUrl = `https://graph.instagram.com/${GRAPH_VERSION}/${nonReel.id}/comments?fields=id,text,timestamp&limit=1&access_token=${token}`;
+    const commentsRes = await fetch(commentsUrl).then((r) => r.json()).catch((e) => ({ error: String(e) }));
+    nonReelComment = commentsRes;
+    const firstCommentId = commentsRes.data && commentsRes.data[0] && commentsRes.data[0].id;
+    if (firstCommentId) {
+      const replyUrl = `https://graph.instagram.com/${GRAPH_VERSION}/${firstCommentId}/private_replies?access_token=${token}`;
+      nonReelReplyTest = await fetch(replyUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ message: { text: "Test" } }),
+      }).then((r) => r.json()).catch((e) => ({ error: String(e) }));
+    }
+  }
 
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ debugInfo, jsonBodyReplyTest: replyRes }, null, 2),
+    body: JSON.stringify(
+      {
+        debugInfo,
+        mediaTypeCounts: (mediaRes.data || []).reduce((acc, m) => {
+          acc[m.media_product_type] = (acc[m.media_product_type] || 0) + 1;
+          return acc;
+        }, {}),
+        nonReelFound: nonReel || null,
+        nonReelComment,
+        nonReelReplyTest,
+      },
+      null,
+      2
+    ),
   };
 };
